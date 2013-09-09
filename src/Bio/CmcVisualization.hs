79bf856afcc3ff5766bdfaca42e7580746880dcf
Florian Eggenhofer <egg@tbi.univie.ac.at>
2013-09-09 13:25:48 +0200

    Added external libaries

---
+++ src/Bio/CmcVisualization.hs
@@ -0,0 +1,13 @@
+-- | Visualize detailed comparsions of Infernal Covariance Models
+--   Datastructures and parsing of covariance models is provided by Biobase
+--   Visualization is accomplished with diagrams-svg
+--   For more information on Infernal consult <http://meme.nbcr.net/meme/>
+
+module Main where
+
+import Diagrams
+import Biobase.SElab.CM
+import Biobase.SElab.CM.Import
+import Biobase.SElab.HMM
+import Biobase.SElab.HMM.Import
+
