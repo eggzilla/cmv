0b423d8f529f4d0457854e4bdefa57743e1cb617
Florian Eggenhofer <egg@tbi.univie.ac.at>
2013-09-11 18:01:17 +0200

    Added basic input processing

--- src/Bio/CmcVisualization.hs
+++ src/Bio/CmcVisualization.hs
@@ -1,3 +1,7 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE RecordWildCards #-}
+
 -- | Visualize detailed comparsions of Infernal Covariance Models
 --   Datastructures and parsing of covariance models is provided by Biobase
 --   Visualization is accomplished with diagrams-svg
@@ -5,9 +9,53 @@
 
 module Main where
 
-import Diagrams
+import Diagrams.Prelude
+import Diagrams.Backend.SVG
+import Control.Monad
+    
+import Biobase.Primary
 import Biobase.SElab.CM
 import Biobase.SElab.CM.Import
-import Biobase.SElab.HMM
-import Biobase.SElab.HMM.Import
 
+import System.Console.CmdArgs
+import System.Environment (getArgs)
+import Text.Printf
+
+data Options = Options            
+  { cmcompareResultFile :: CmcompareResultFile,
+    models :: [String]
+  } deriving (Show,Data,Typeable)
+
+type  CmcompareResultFile = String
+             
+data CmcompareResult = CmcompareResult           
+  { model1Name :: String,
+    model2Name :: String,
+    linkscore1 :: Double,
+    linkscore2 :: Double,
+    linksequence  :: String,
+    model1structure :: String,
+    model2structure :: String,
+    model1matchednodes :: String,
+    model2matchednodes :: String
+  } deriving (Show,Data,Typeable)
+
+             
+options = Options
+  { cmcompareResultFile = def &= help "CMCompare Result string",
+    models = def &= args -- &= help "path to exactly two covariance models"
+  } &= summary "CMCV devel version" &= help "2013, egg" &= verbosity
+
+
+main = do
+  Options{..} <- cmdArgs options
+  unless (length models == 2) $ do
+    fail "give exactly two CMs"
+  let [a,b] = models
+  let c = cmcompareResultFile
+  [theA] <- fromFile a
+  [theB] <- fromFile b
+  cmcresult <- fromFile c         
+  print [theA]
+  print [theB]
+  print cmcresult     
