3b0bb5d0339900f11b9780a8a0c97d750a5f670b
Florian Eggenhofer <egg@tbi.univie.ac.at>
2013-09-19 14:58:20 +0200

    Added drawing of CM guide tree nodes

--- src/Bio/CmDraw.hs
+++ src/Bio/CmDraw.hs
@@ -6,12 +6,18 @@ module CmDraw
 import Diagrams.Prelude
 import Diagrams.TwoD
 import Diagrams.Backend.SVG
+import Graphics.SVGFonts.ReadFont
 
-drawSVG = circle 1
+-- | Draw the guide Tree of a single CM, utilizes drawCMGuideNode
+--drawCMGuideTree :: CM -> 
+
+-- | Draws the guide tree nodes of a CM
+drawCMGuideNode label number =  text' label # translate (r2 (0,20)) <> text' number # translate (r2 (0,-20)) <> rect 100 100 # lw 1 # fc lightgrey
+text' t = stroke (textSVG t 40) # fc black # fillRule EvenOdd
 
 --print svg          
 svgwidth = Just 100
 svglength = Just 100            
-svgsize = mkSizeSpec svgwidth svglength                            
-printSVG = renderSVG "./testdiagram.svg" svgsize drawSVG
-
+svgsize = mkSizeSpec svgwidth svglength
+testdrawing = drawCMGuideNode "test" "1" ||| drawCMGuideNode "test" "2"||| drawCMGuideNode "test" "3"||| drawCMGuideNode "test" "4"  
+printSVG = renderSVG "./testdiagram.svg" svgsize testdrawing
--- src/Bio/CmcVisualization.hs
+++ src/Bio/CmcVisualization.hs
@@ -30,17 +30,17 @@ options = Options
   { cmcompareResultFile = def &= name "r" &= help "Path to CMCompare result file",
     modelsFile = def &= name "m" &= help "Path to covariance model file"
   } &= summary "CMCV devel version" &= help "Florian Eggenhofer - 2013" &= verbosity
-
-
                
 main = do
   Options{..} <- cmdArgs options
   let a = modelsFile
   let b = cmcompareResultFile
   models <- fromFile a
-  print a
-  print b
+  --print a
+  --print b
   cmResultParsed <- getCmcompareResults b
+
+  
   --let width = Just 100
   --let length = Just 100         
   --let testdiagramsize = mkSizeSpec width length                   
