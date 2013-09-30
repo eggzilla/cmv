1cf7234e7a436980749776a9ecd1105cdae1d110
Florian Eggenhofer <egg@tbi.univie.ac.at>
2013-09-30 08:07:53 +0200

    Fixed missing linewidth of highlighting area and code cleanup

--- src/Bio/CmDraw.hs
+++ src/Bio/CmDraw.hs
@@ -17,37 +17,46 @@ import Diagrams.Path
 import Diagrams.Backend.SVG
 import Data.Colour.SRGB
 import Graphics.SVGFonts.ReadFont
-
---ComparisonBoundry = data ComparisonBoundry{
-    
+  
 -- | Draw one or more CM guide trees and concatenate them vertically
+--drawCMGuideForest :: (Renderable a b) => [[(String,String)]] -> QDiagram b0 R2 Any
 drawCMGuideForest cms = alignTL (vcat' with { sep = 40 } (drawCMGuideTrees cms)) <> highlightComparisonTrail 1 5 2 10 1 50 2 80 
 
-                        
 -- | Highlight comparison by connecting the delimiting nodes of the aligned nodes of both models
 -- takes the model identifier of both models and the starting and ending nodes of both models as arguments.
+--highlightComparisonLines ::
 highlightComparisonLines a b c d e f g h = highlightComparisonIntervalBoundry a b c d <> highlightComparisonIntervalBoundry e f g h
+
+-- | Highlight comparison by connecting the all of the aligned nodes of both models
+--highlightComparisonTrail ::
 highlightComparisonTrail a b c d e f g h = connectionTrail (getNodeCoordinates a b) (getNodeCoordinates c d) (getNodeCoordinates e f) (getNodeCoordinates g h)
+
+--highlightComparisonIntervalBoundry ::
 highlightComparisonIntervalBoundry model1index node1index model2index node2index = connectionLine (getNodeCoordinates model1index node1index) (getNodeCoordinates model2index node2index)
 
--- | returns the center coordinates for an Covariance model guide tree node
+-- | Returns the center coordinates for an Covariance model guide tree node
 getNodeCoordinates :: Int -> Int -> P2
 getNodeCoordinates modelindex nodeindex = p2 (fromIntegral x, fromIntegral y)
    where y = (getYCoordinate modelindex 0) * (-1)
          x = (5 + (10 * (nodeindex - 1 )))
 
+-- |  Computes the y coodinate for comparison highlighting, so that the
+-- line or area starts at the lower edge of the cm representation and ends right above it
+getYCoordinate :: Int -> Int -> Int 
 getYCoordinate modelindex ycoordinate
     | modelindex == 0 = ycoordinate 
     | even modelindex = getYCoordinate (modelindex - 1) (ycoordinate + 40) 
     | odd modelindex = getYCoordinate (modelindex - 1) (ycoordinate + 10) 
 
+
 --connectionLine :: Int-> Int -> Int -> Int -> 
 connectionLine a b = fromVertices [a,b] # lw 0.5 # lc green
-connectionTrail a b c d = stroke (paralellogram a b c d ) # fc aqua # fillRule EvenOdd # lc grey
+
+--connectionTrail ::
+connectionTrail a b c d = stroke (paralellogram a b c d ) # fc aqua # fillRule EvenOdd # lc grey # lw 0.5
 mkPath a b c d = position [a,b,c,d,a]
               
 paralellogram :: P2 -> P2 -> P2 -> P2 -> Path R2
---paralellogram a b c d = fromVertices [a,b,d,c,a] 
 paralellogram a b c d = pathFromTrailAt (closeTrail (trailFromVertices [a,b,d,c,a])) a
 
 -- | Draw the Guide Trees of multiple CMs, utilizes drawCMGuideNode
@@ -83,16 +92,7 @@ labelToColor label
 labelToColor _ = sRGB24 245 245 245
           
 --scaling
-
--- | Width of the drawing is determinded by the model with the max node number
-svgwidth :: [[(String,String)]] -> Maybe Double
-svgwidth processedCMs = Just (fromIntegral 1) --Just (fromIntegral (maximum (map length processedCMs)))
-
--- | Length of the diagram is determined by the number of compared models
-svglength :: [[(String,String)]] -> Maybe Double
-svglength processedCMs = Just (fromIntegral (length processedCMs))
-
--- | Specifies the size of the diagram          
+-- | Specifies the size of the diagram. Absolute adapts to overall size according to subdiagrams
 --svgsize processedCMs = mkSizeSpec (svgwidth processedCMs) (svglength processedCMs)
 svgsize = Absolute
 diagramName = "./testdiagram.svg"
