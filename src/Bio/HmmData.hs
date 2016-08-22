aa0bb04bc12c40b45dfc492adc0cbcda68518b6a
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2016-08-22 19:53:07 +0200

    Extended hmmer datatype and added some documentation

--- src/Bio/HmmData.hs
+++ src/Bio/HmmData.hs
@@ -6,24 +6,49 @@ module Bio.HmmData where
 import qualified Data.Vectors as V
 
 -- | Data structure for nHMMER
-data Nhmmer = Nhmmer
-  { 
+data Nhmmer3 = Nhmmer3
+  {
+    -- file format version; mandatory
     version :: String,
+    -- model name; mandatory
     name :: String,
-    acc :: String,
+    -- accession number; optional
+    acc :: Maybe String,
+    -- description; optional
+    desc :: Maybe String,
+    -- model length; mandatory
     leng :: Int,
-    maxl :: Int,
+    -- maximum instance length; optional
+    maxl :: Maybe Int,
+    -- symbol alphabet type either amino, RNA or DNA; mandatory
     alpha :: String,
+    -- reference annotation; optional
     rf :: Bool,
+    -- model masked; optional
     mm :: Bool,
+    -- consensus residue annotation flag; Mandatory
     cons :: Bool,
-    cs :: Bool,
+    -- consensus structure; optional
+    cs :: Maybe Bool,
+    -- map annotation; optional
     map :: Bool,
+    -- date; optional
     date :: String,
-    com :: String,
+    -- command line log; optional
+    com :: Maybe String,
+    -- sequence number; optional
     nseq :: Int,
+    -- effective sequence number; optional
     effn :: Double,
+    -- training alignment checksum; optional
     cksum :: Int,
+    -- Pfam gathering treshold; optional
+    ga :: Maybe (Double, Double),
+    -- Pfam trusted cutoff; optional
+    tc :: Maybe (Double, Double),
+    -- Pfam noise cutoff; optional
+    nc :: Maybe (Double, Double),
+    --stats
     localmsv :: (Double,Double),
     localviterbi :: (Double,Double),
     localforward :: (Double,Double),
