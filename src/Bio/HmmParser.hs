55f27f067b8d728f2db1f4395e1a9f2d0db6216d
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2016-08-23 11:49:29 +0200

    Added a prototype for a HMMER3 parser

---
+++ src/Bio/HmmParser.hs
@@ -0,0 +1,30 @@
+-- | Parse HMM models
+--   Currently contains a parser for HMMER3 models
+--   For more information on HMMER consult: <http://hmmer.org>
+module Bio.HMMParser (
+                       parseHMMER3,
+                       readHMMER3,                                   
+                       module Bio.HMMData
+                      ) where
+
+import Bio.HMMData
+import Text.ParserCombinators.Parsec
+import qualified Control.Exception.Base as CE
+
+-- | parse HMMER3 model from input string
+parseHMMER3 :: [Char] -> Either ParseError HMMER3
+parseHMMER3 input = parse genParseHMMER3 "HMMER3" input
+
+-- | parse HMMER3 from input filePath                      
+readHMMER3 :: String -> IO (Either ParseError HMMER3)                  
+readHMMER3 filePath = do 
+  parsedFile <- parseFromFile genParseHMMER3 filePath
+  CE.evaluate parsedFile
+
+-- | Parse the input as HMMER3 datatype
+genParseHMMER3 :: GenParser Char st HMMER3
+genParseHMMER3 = do
+  _version <- many1 (noneOf " ")      
+  return $ HMMER3 _version 
+
+         
