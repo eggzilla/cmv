8df333baf681584a9e24477e06b3cfdaef973cf8
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2016-08-25 17:14:11 +0200

    further changes

--- src/Bio/HMMParser.hs
+++ src/Bio/HMMParser.hs
@@ -25,109 +25,99 @@ readHMMER3 filePath = do
 -- | Parse the input as HMMER3 datatype
 genParseHMMER3 :: GenParser Char st HMMER3
 genParseHMMER3 = do
-  string "HMMER3/"
-  _version <- many1 (noneOf "\n")
-  newline
-  string "NAME  "
-  _name <- many1 alphaNum
-  newline
-  string "ACC   "
-  _acc  <- many1 alphaNum
-  newline
-  string "DESC  "
-  _desc <- many1 (noneOf "\n")
-  newline
-  string "LENG  "
-  _leng <- parseIntegral
-  newline
-  string "MAXL  "
-  _maxl <- parseIntegral
-  newline
-  string "ALPH  "
-  _alpha <- many1 letter
-  newline
-  string "RF    "
-  _rf <- parseSwitch
-  newline
-  string "MM    "
-  _mm <- parseSwitch
-  newline
-  string "CONS  "
-  _cons <- parseSwitch
-  newline
-  string "CS    "
-  _cs <- parseSwitch
-  newline
-  string "MAP   "
-  _map <- parseSwitch
-  newline
-  string "DATE  "
-  _date <- many1 (noneOf "\n")
-  newline
-  string "COM   "
-  _com <- many1 (noneOf "\n")
-  newline
-  string "NSEQ  "
-  _nseq <- parseIntegral
-  newline
-  string "EFFN  "
-  _effn <- parseFloat
-  newline
-  string "CKSUM "
-  _cksum <- parseIntegral
-  newline
-  string "GA    "
-  _ga <- parseFloat
-  newline
-  string "TC    "
-  _tc <- parseFloat
-  newline
-  string "NC    "
-  _nc <- parseFloat
-  newline
-  string "STATS LOCAL MSV       "
-  _localmsv <- parseStat
-  newline
-  string "STATS LOCAL VITERBI   "
-  _localviterbi <- parseStat
-  string "STATS LOCAL FORWARD   "
-  _localforward <- parseStat
+  _version <- parseStringAttribute "HMMER3/"
+  _name <- parseStringAttribute "NAME"
+  _desc <- parseStringAttribute "DESC"
+  _leng <- parseIntAttribute "LENG"
+  --string "LENG  "
+  --_leng <- parseIntegral
+  --newline
+  _maxl <- parseIntAttribute "MAXL"
+  --string "MAXL  "
+  --_maxl <- parseIntegral
+  --newline
+  _alph <- parseStringAttribute "ALPH"
+  --string "ALPH  "
+  --_alpha <- many1 letter
+  --newline
+  _rf <- parseSwitchAttribute "RF"
+  _nc <- parseSwitchAttribute "MM"
+  _nc <- parseSwitchAttribute "CONS"
+  _nc <- parseSwitchAttribute "CS"
+  _nc <- parseSwitchAttribute "MAP"
+  _nc <- parseStringAttribute "DATE"
+  _nc <- parseStringAttribute "COM"
+  _nc <- parseIntAttribute "NSEQ"
+  _nc <- parseFloatAttribute "EFFN"
+  _nc <- parseIntAttribute "CKSUM "
+  _nc <- parseFloatAttribute "GA"
+  _nc <- parseFloatAttribute "TC"
+  _nc <- parseFloatAttribute "NC"
+  _localmsv <- parseStatAttribute "STATS LOCAL MSV"
+  _localviterbi <- parseStatAttribute "STATS LOCAL VITERBI"
+  _localforward <- parseStatAttribute "STATS LOCAL FORWARD"
   string "HMM"
   _hmm <- many1 parseAlphabetSymbol
   newline
-  _compo <- parseCOMPOsite
-  _nodes <- many1 parseNodes
+  _compo <- parseHMMER3Node
+  _nodes <- many1 parseHMMER3Node
   string "//"
   eof
-  return $ HMMER3 _version _name _acc _desc _leng _maxl _alpha _rf _mm _cons _cs _map _date _com _nseq _effn _cksum _ga _tc _nc _localmsv _localviterbi _localforward _hmm _compo _nodes
+  return $ HMMER3 _version _name _acc _desc _leng _maxl _alpha _rf _mm _cons _cs _map _date _com _nseq _effn _cksum _ga _tc _nc _localmsv _localviterbi _localforward (V.fromList _hmm) _compo _nodes
 
-parseSwitch :: GenParser Char st Bool
-parseSwitch = do
+parseSwitchAttribute :: String -> GenParser Char st Bool
+parseSwitchAttribute fieldName = do
+  string fieldName
   many1 (oneOf " ")
-  _switch <- string "Yes" <|> string "No"
-  return switch
+  _switch <- try (string "Yes") <|> try (string "No")
+  return switchToBool _switch
 
 switchToBool :: String -> Bool
 switchToBool switch
   | switch == "yes" = True
   | otherwise = False
 
-parseStat :: GenParser Char st (Double,Double)
-parseStat = do
+parseStringAttribute :: String ->  GenParser Char st String
+parseStringAttribute fieldName = do
+  string fieldName
+  many1 (oneOf " ")
+  _string <- many1 (noneOf "\n")
+  newline
+  return string
+
+parseFloatAttribute :: String ->  GenParser Char st Double
+parseFloatAttribute fieldName = do
+  string fieldName
+  many1 (oneOf " ")
+  _double <- many1 (noneOf "\n")
+  newline
+  return double
+
+parseIntAttribute :: String ->  GenParser Char st Int
+parseIntAttribute fieldName = do
+  string fieldName
+  many1 (oneOf " ")
+  _int <- parseIntegral
+  newline
+  return int
+
+parseStatAttribute :: String -> GenParser Char st (Double,Double)
+parseStatAttribute fieldName = do
+  string fieldName
   many1 (oneOf " ")
   _stat1 <- parseFloat
   many1 (oneOf " ")
   _stat2 <- parseFloat
-  return (stat1,stat2)
+  return (_stat1,_stat2)
   
 parseAlphabetSymbol :: GenParser Char st Char
 parseAlphabetSymbol = do
   many1 (oneOf " ")
-  _symbol <- uppercase
-  return symbol
+  _symbol <- upper
+  return _symbol
   
 -- | Parse HMMER3 node
-parseHMMER3Node :: GenParser Char st HMMER3node
+parseHMMER3Node :: GenParser Char st HMMER3Node
 parseHMMER3Node = do
   _nodeId <- many1 (noneOf "\n")
   _matchEmissions <- many1 (noneOf "\n")
