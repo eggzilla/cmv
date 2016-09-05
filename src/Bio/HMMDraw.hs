eeb512efc487ecf726ff58fefc3a909d4b8d715a
Florian Eggenhofer <egg@tbi.univie.ac.at>
2016-09-05 18:54:52 +0200

    Fixed multiple text and box alignment issues

--- src/Bio/HMMDraw.hs
+++ src/Bio/HMMDraw.hs
@@ -37,12 +37,13 @@ drawHMMMER3s modelDetail hmms
 drawHMMER3 modelDetail model
    | modelDetail == "flat" = hcat (map drawHMMNodeFlat currentnodes)
    | modelDetail == "simple" = hcat (map drawHMMNodeSimple currentnodes)
-   | modelDetail == "detailed" = hcat (map (drawHMMNodeVerbose alphabet "bar") currentnodes) <> rect (5 * nodenumber) (boxlength + 8)  # fc white # lw 0 # translate (r2 (((5 * nodenumber)/2 ), (negate (boxlength + 7)/2)))
+   | modelDetail == "detailed" = hcat (map (drawHMMNodeVerbose alphabet "probability") currentnodes) <> background
    | otherwise = hcat (map drawHMMNodeSimple currentnodes)
      where nodenumber = fromIntegral $ length currentnodes
            currentnodes = HM.nodes model
            alphabet = (HM.alpha model)
            boxlength = 2 * (fromIntegral (length alphabet))
+           background = rect (5 * nodenumber) (boxlength + 9)  # fc white # lw 0 # translate (r2 (((5 * nodenumber)/2 -2), (negate (boxlength + 7)/2 + 2)))
 -- | 
 --drawHMMNodeFlat :: forall t b. (Data.Typeable.Internal.Typeable (N b), TrailLike b, HasStyle b, V b ~ V2) => HM.HMMER3Node -> b
 drawHMMNodeFlat node = rect 2 2 # lw 0.1  
@@ -54,11 +55,11 @@ drawHMMNodeSimple node =  rect 2 2 # lw 0.1
 -- | 
 --drawHMMNodeVerbose :: String -> String -> HM.HMMER3Node -> QDiagram b V2 n Any
 drawHMMNodeVerbose alphabet emissiontype node = deletions === strutY 1 === insertions === strutY 1 === matches alphabet emissiontype node ||| strutX 1
-deletions =  alignedText (negate 0.5) (negate 0.5) "D" <> circle 1 # lw 0.1 # fc white
-insertions = alignedText (negate 0.5) (negate 0.5) "I" <> rect 2 2 # lw 0.1 # rotateBy (1/8) # fc white 
+deletions =  alignedText 0 0 "D" # translate (r2 (negate 0.25,negate 0.25)) <> circle 1 # lw 0.1 # fc white
+insertions = alignedText 0 0 "I" # translate (r2 (negate 0.25,negate 0.25)) <> rect 2 2 # lw 0.1 # rotateBy (1/8) # fc white 
 
 
-matches alphabet emissiontype node = entries # translate (r2 (negate 1, boxlength/2)) <> outerbox
+matches alphabet emissiontype node = entries # translate (r2 (negate 1, boxlength/4)) <> outerbox
   where outerbox = rect 4 boxlength # lw 0.1 # fc white
         entries = vcat (map (emissionEntry emissiontype) symbolsAndEmissions)
         symbolsAndEmissions = zip (map wrap alphabetSymbols) emissionEntries
@@ -83,12 +84,12 @@ emissionEntry emissiontype (symbol,emission)
   | emissiontype == "probability" = textentry
   | emissiontype == "score" = textentry
   | emissiontype == "bar" = barentry
-    where textentry = alignedText 0 0.1 (symbol ++ " " ++ printf "%.3f" emission)
+    where textentry = alignedText 0 0.1 (symbol ++ " " ++ printf "%.3f" emission) # translate (r2 (negate 0.5,0)) <> (rect 2 1 # lw 0 ) 
           --barentry =  stroke (textSVG symbol 2) ||| bar emission
-          barentry = (alignedText 0 0.1  symbol <> (rect 2 1 # lw 0 ) )||| bar emission
+          barentry = (alignedText 0 0.01  symbol <> (rect 2 1 # lw 0 ) )||| bar emission
 
 --bar :: forall b n. (Read n, RealFloat n, Data.Typeable.Internal.Typeable n, Renderable (Path V2 n) b) => Double -> QDiagram b V2 n Any
-bar emission = rect 1 1 # lw 0.1  ||| rect emission 1 # lw 0 # fc black # translate (r2 (negate 1, 0.0))
+bar emission = (rect emission 1 # lw 0 # fc black # translate (r2 (negate (0.5 - (emission/2)),0)) <> rect 1 1 # lw 0.01 )
 
 
 -- | 
