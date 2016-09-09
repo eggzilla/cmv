4001c719c587ca7a13d70c4aeca3f45833541615
Florian Eggenhofer <egg@informatik.uni-freiburg.de>
2016-09-09 15:55:50 +0200

    Added reading of stockholm alignment to hmmv

--- src/hmmv/HMMVisualisation.hs
+++ src/hmmv/HMMVisualisation.hs
@@ -12,6 +12,7 @@ import Bio.HMMDraw
 import System.Console.CmdArgs
 import Data.Either.Unwrap
 import System.Directory
+import Bio.StockholmParser
 
 options :: Options
 data Options = Options            
@@ -23,6 +24,7 @@ data Options = Options
 
 options = Options
   { modelFile = def &= name "m" &= help "Path to hidden Markov model file",
+    alignmentFile = "" &= name "s" &= help "Path to stockholm alignment file",
     modelDetail = "detailed" &= name "d" &= help "Set verbosity of drawn models: simple, detailed",
     modelLayout = "flat" &= name "l" &= help "Set layout of drawn models: flat, tree"
   } &= summary "HMMvisualisation devel version" &= help "Florian Eggenhofer - 2016" &= verbosity
@@ -31,10 +33,17 @@ main :: IO ()
 main = do
   Options{..} <- cmdArgs options
   modelFileExists <- doesFileExist modelFile
+  alnFileExists <- doesFileExist alignmentFile
   if modelFileExists
      then do
        model <- HM.readHMMER3 modelFile
        --print model
-       if (isRight model) then printSVG svgsize (drawHMMER3 modelDetail (fromRight model)) else print (fromLeft model)
+       if alnFileExists
+          then do
+            aln <- readStockholm alignmentFile
+            if (isRight aln) then print (fromRight aln) else print (fromLeft aln)
+            if (isRight model) then printSVG svgsize (drawHMMER3 modelDetail (fromRight model)) else print (fromLeft model)
+       else do     
+         if (isRight model) then printSVG svgsize (drawHMMER3 modelDetail (fromRight model)) else print (fromLeft model)
      else do
        putStrLn "Input model file not found"
