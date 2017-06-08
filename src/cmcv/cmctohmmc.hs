d3f72a92106eee175ed15fe9dbffedadcb5f4ee0
Florian Eggenhofer <egg@tbi.univie.ac.at>
2017-06-09 01:08:14 +0200

    Some additions to cmctohmmc

--- src/cmcv/cmctohmmc.hs
+++ src/cmcv/cmctohmmc.hs
@@ -26,13 +26,13 @@ import Data.Maybe
 import qualified Data.Map as M
 import qualified Bio.HMMCompareResult as HC
 import qualified Data.Vector as V
+import Data.List
 
 options :: Options
 data Options = Options
   { cmcompareResultFile :: String,
     cmmodelsFile :: String,
-    hmmmodelsFile :: String,
-    outputDirectoryPath :: String
+    hmmmodelsFile :: String
   } deriving (Show,Data,Typeable)
 
 options = Options
@@ -46,12 +46,12 @@ main = do
   Options{..} <- cmdArgs options
   --Validate input
   cmmodelFileExists <- doesFileExist cmmodelsFile
-  hmmodelFileExists <- doesFileExist hmmmodelsFile
+  hmmmodelFileExists <- doesFileExist hmmmodelsFile
   cmcFileExists <- doesFileExist cmcompareResultFile
-  if cmmodelFileExists && hmmodelFileExists && cmcFileExists
+  if cmmodelFileExists && hmmmodelFileExists && cmcFileExists
      then do
-       cms <- cmFromFile modelsFile
-       inputhmms <- HM.readHMMER3 modelsFile
+       cms <- cmFromFile cmmodelsFile
+       inputhmms <- HM.readHMMER3 hmmmodelsFile
        let hmms = E.fromRight inputhmms
        cmcResultParsed <- getCmcompareResults cmcompareResultFile
        let comparisons = rights cmcResultParsed
@@ -59,11 +59,10 @@ main = do
        let hmmVector = V.fromList hmms
        --let modelNumber = length cms
        let cmModelNames = map (T.unpack . CM._name) cms
-       let hmmModelNames = map HM.name models
-       currentWD <- getCurrentDirectory
-       let dirPath = if null outputDirectoryPath then currentWD else outputDirectoryPath
-       let modelFileNames = map (\m -> m ++ "." ++ outputFormat) currentModelNames
-       setCurrentDirectory dirPath
+       let hmmModelNames = map HM.name hmms
+       --currentWD <- getCurrentDirectory
+       --let dirPath = if null outputDirectoryPath then currentWD else outputDirectoryPath
+       --setCurrentDirectory dirPath
        let hmmcs = map (cmctohmmv cmModelNames hmmModelNames cmVector hmmVector) comparisons
        print hmmcs
      else do
@@ -76,30 +75,35 @@ toolVersion = showVersion version
 
 cmctohmmv :: [String] -> [String] -> V.Vector CM.CM -> V.Vector HM.HMMER3 -> CmcompareResult -> HC.HMMCompareResult -- > HMMCompareResult
 cmctohmmv cmModelNames hmmModelNames cmVector hmmVector comparison = hmmcompare
-  where cm1Index = elemIndex model1Name comparisons
-        cm2Index = elemIndex model2Name comparisons
-        cm1 = cmVector V.! cm1Index
-        cm2 = cmVector V.! cm2Index
+  where cm1Index = fromJust (elemIndex (model1Name comparison) cmModelNames)
+        cm2Index = fromJust (elemIndex (model2Name comparison) cmModelNames)
+        inputCM1 = cmVector V.! cm1Index
+        cm1 = E.fromLeft (CM._cm inputCM1)     
+        inputCM2 = cmVector V.! cm2Index
+        cm2 = E.fromLeft (CM._cm inputCM2)
         hmm1 = hmmVector V.! cm1Index
         hmm2 = hmmVector V.! cm2Index
         cm1nodeIndices = nub (model1matchednodes comparison)
         cm2nodeIndices = nub (model2matchednodes comparison)
-        cm1nodes = V.fromList (M.elems (CM._fmNodes cm1))
-        cm2nodes = V.fromList (M.elems (CM._fmNodes cm2))
+        cm1nodes = (M.elems (CM._fmNodes cm1))
+        cm2nodes = (M.elems (CM._fmNodes cm2))
         hmm1nodes = HM.nodes hmm1
         hmm2nodes = HM.nodes hmm2
-        aln1colIndices = concatMap nodeToColumnIndex cm1nodes
-        aln2colIndices = concatMap nodeToColumnIndex cm1nodes
+        --filter cmnodes
+        intervalcm1nodes =
+        intervalcm1nodes =
+        aln1colIndices = concatMap nodeToColumnIndices cm1nodes
+        aln2colIndices = concatMap nodeToColumnIndices cm2nodes
         linkedhmm1nodes = V.filter (\node -> elem (fromJust (HM.nma node)) aln1colIndices) hmm1nodes
-        linkedhmm2nodes = V.filter (\node -> elem (fromJust (HM.nma node)) aln1colIndices) hmm2nodes
+        linkedhmm2nodes = V.filter (\node -> elem (fromJust (HM.nma node)) aln2colIndices) hmm2nodes
         hmm1interval = V.map HM.nodeId linkedhmm1nodes
         hmm2interval = V.map HM.nodeId linkedhmm2nodes
-        hmmcompare = HC.HMMCompareResult (model1Name comparison) (model2Name comparison) (linkscore1 comparison) (linkscore2 comparison) (linksequence comparison) hmm1interval hmm2interval
+        hmmcompare = HC.HMMCompareResult (model1Name comparison) (model2Name comparison) (linkscore1 comparison) (linkscore2 comparison) (linksequence comparison) (V.toList hmm1interval) (V.toList hmm2interval)
 
-nodeToColumnIndices:: V.Vector CM.Node -> Int -> [Int]
-nodeToColumnIndices nodes nodeIndex = colLabels
-  where currentNode = (V.!) nodes (nodeIndex)
-        colIndices = nub [CM._nodeColL currentNode,CM._nodeColR currentNode]
+nodeToColumnIndices:: CM.Node -> [Int]
+nodeToColumnIndices currentNode = nub [CM._nodeColL currentNode,CM._nodeColR currentNode]
+--  where currentNode = (V.!) nodes (nodeIndex)
+--        colIndices = nub [CM._nodeColL currentNode,CM._nodeColR currentNode]
 
 writeModelNameFile :: Bool -> String -> [String] -> IO ()
 writeModelNameFile toggle outputDirectoryPath modelNames =
